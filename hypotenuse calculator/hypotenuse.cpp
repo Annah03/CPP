ea40d496e8192a934506a573fbb50b04fd056edd
Annah Singh <annahsingh089@gmail.com>
2025-01-30 08:19:20 -0500

    hypotenuse calculator

---
+++ hypotenuse calculator/hypotenuse.cpp
@@ -0,0 +1,17 @@
+//write a program that can take input and calculate the hypothenuse
+#include <iostream>
+#include <cmath>
+
+//define variables
+double a, b, c;
+
+int main(){
+std::cout << "What is the value of a?"<<std::endl;
+std::cin >> a;
+std::cout << "What is the value of b?"<<std::endl;
+std::cin >> b;
+c = pow(a,2) + pow(b,2);
+c = sqrt(c);
+std::cout << "The hypotenuse is " << c << std::endl;
+return 0;
+}
